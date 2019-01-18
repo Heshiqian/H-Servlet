510870a5d812acf764dec3b2429b1d60dc897693
贺石骞 <heshiqian3@126.com>
2019-01-18 10:42:42 +0800

    移出web目录

--- web/static/js/master.js
+++
@@ -1,6 +0,0 @@
-var mask=$('<div class="mask">\n' +
-    '    <div class="m-laoding">\n' +
-    '        <div class="m-laoding-icon"></div>\n' +
-    '        <p class="m-laoding-p">加载中...</p>\n' +
-    '    </div>\n' +
-'</div>');
